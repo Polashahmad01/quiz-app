2b2373d2e00960a601902b6f18fceace2d414a8c
Polashahmad01 <dev.polashahmad@gmail.com>
2023-12-02 16:35:31 +0600

    Add quiz completion logic and display quiz summary

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 
 import QUESTIONS from "../questions"
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([])
   const activeQuestionIndex = userAnswers.length
-
+  const quizIsComplete = userAnswers.length === QUESTIONS.length
+  
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnserQuestion) => {
       return [
@@ -15,14 +17,24 @@ export default function Quiz() {
     })
   }
   
-  console.log(userAnswers)
+  if(quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    )
+  }
+  
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort(() => Math.random() - 0.5)
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
