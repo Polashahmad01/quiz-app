6478bc52fa12d1bf0e1e7d66ee28ec194e0fffbc
Polashahmad01 <dev.polashahmad@gmail.com>
2023-12-02 18:47:38 +0600

    Add Quiz Summary component

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout)
+
+  useEffect(() => {
+    console.log("SETTIMEOUT")
+    const timer = setTimeout(() => {
+      onTimeout()
+    }, timeout)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [timeout, onTimeout])
+
+  useEffect(() => {
+    console.log("SETINTERVAL")
+    const interval = setInterval(() => {
+      setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <progress id="question-time" max={timeout} value={remainingTime} />
+  )
+}
